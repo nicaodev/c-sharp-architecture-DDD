d24bc29b89c6bf7849420ef1469eff14234440a2
Nicaodev <alexandre.nicos@gmail.com>
2020-12-20 16:55:15 -0300

    feature/1.0.0 - Criando BaseEntity e referencias.

---
+++ Api.Domain/Entities/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Entities
+{
+    public class UserEntity : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ Api.Infrastructure.Data/Context/Context.cs
@@ -0,0 +1,19 @@
+﻿using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Infrastructure.Data.Context
+{
+    public class Context : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
