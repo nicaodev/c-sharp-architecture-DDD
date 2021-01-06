30838a9253bc7d7b4c8e57225cfe319b87aa79c9
Nicaodev <alexandre.nicos@gmail.com>
2021-01-05 22:52:28 -0300

    feature/1.3.0 - service de login

---
+++ Api.Infrastructure.Data/Concrete/UserLogin.cs
@@ -0,0 +1,28 @@
+﻿using Api.Domain.Entities;
+using Api.Domain.Repository;
+using Api.Infrastructure.Data.Context;
+using Api.Infrastructure.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Infrastructure.Data.Concrete
+{
+    public class UserLogin : BaseRepository<UserEntity>, IUserRepository
+    {
+        private DbSet<UserEntity> _dataset;
+
+        public UserLogin(ContextDb context) : base(context)
+        {
+            _dataset = context.Set<UserEntity>();
+        }
+
+        public async Task<UserEntity> FindByLogin(string email)
+        {
+            return await _dataset.FirstOrDefaultAsync(a => a.Email.Equals(email));
+        }
+    }
+}
+
---
+++ Api.Service/Services/LoginService.cs
@@ -0,0 +1,32 @@
+﻿using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.User;
+using Api.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Service.Services
+{
+    public class LoginService : ILoginService
+    {
+        private IUserRepository _repo;
+
+        public LoginService(IUserRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<object> FindByLogin(UserEntity user)
+        {
+
+            if (user != null && !string.IsNullOrWhiteSpace(user.Email))
+            {
+                return await _repo.FindByLogin(user.Email);
+
+            }
+            else
+                return null;
+
+        }
+    }
+}
