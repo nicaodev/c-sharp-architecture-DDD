f7cfd8469bcfd3eee8511806aa0fab0d3a403188
Nicaodev <alexandre.nicos@gmail.com>
2021-01-07 23:11:18 -0300

    feature/1.3.1 - Criando Dtos

--- Api.Domain/DTOs/LoginDto.cs
+++ Api.Domain/DTOs/LoginDto.cs
@@ -4,8 +4,8 @@ namespace Api.Domain.DTOs
 {
     public class LoginDto
     {
-        [Required(ErrorMessage = "Nome é um campo obrigatório.")]
-        public string Name { get; set; }
+        //[Required(ErrorMessage = "Nome é um campo obrigatório.")]
+        //public string Name { get; set; }
 
         [EmailAddress(ErrorMessage = "E-mail em formato inválido.")]
         public string Email { get; set; }
---
+++ Api.Domain/DTOs/User/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.DTOs.User
+{
+    public class UserDto
+    {
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "E-mail não está válido.")]
+        public string Email { get; set; }
+    }
+}
